42443eddcef8ef0dc839138f4275e5c44afc6fda
johnom-commits <johnom2012@yandex.ru>
2019-08-28 20:12:23 +0300

    Max value

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+
+    public int max(int first, int second, int third) {
+        int result;
+        result = first > second ? first : second;
+        result = result > third ? result : third;
+        return result;
+    }
+}
