3c140db2463a5e8713330d0666f740962b48fef2
johnom-commits <johnom2012@yandex.ru>
2019-08-29 20:24:59 +0300

    Prime number

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i <= finish; i++){
+            int flag = 1;
+            for (int j = 2; j <= finish; j++){
+                if (i != j && i % j == 0) {
+                    flag = 0;
+                }
+            }
+            count += flag;
+        }
+        return count;
+    }
+}
