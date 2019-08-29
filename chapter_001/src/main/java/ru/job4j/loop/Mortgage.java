6834c8f89d4bfe1422722284c9cb58c6f3f1436b
johnom-commits <johnom2012@yandex.ru>
2019-08-29 21:06:39 +0300

    Mortgage

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int monthly, double percent) {
+        int year = 0;
+        double mustToPay = amount + (amount * percent / 100);
+        while (mustToPay > 0){
+            mustToPay -= 12 * monthly;
+            year ++;
+        }
+        return year;
+    }
+}
