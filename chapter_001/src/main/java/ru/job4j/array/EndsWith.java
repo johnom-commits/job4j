dfd92f8993eecc5c5b30dec3692d001383f64432
johnom-commits <johnom2012@yandex.ru>
2019-08-30 19:02:22 +0300

    endsWith

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param post префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean endsWith(String word, String post) {
+        boolean result = true;
+        char[] pst = post.toCharArray();
+        char[] wrd = word.toCharArray();
+        int dif = wrd.length - pst.length;
+
+        for (int i = pst.length-1; i >= 0; i--){
+            if (pst[i] != wrd[i + dif]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
