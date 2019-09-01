b02671866178b45f859ff0e8822dd3f1d03fdbdf
johnom-commits <johnom2012@yandex.ru>
2019-09-01 13:47:43 +0300

    MatrixCheck4

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,12 +3,10 @@ package ru.job4j.array;
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
-        boolean flag = true;
-        boolean flag2 = true;
         int length = data.length - 1;
 
-        flag = data[0][0];
-        flag2 = data[length][0];
+        boolean flag = data[0][0];
+        boolean flag2 = data[length][0];
 
         for (int i = 0; i <= length; i++) {
             if (flag != data[i][i] || flag2 != data[length - i][i]) {
