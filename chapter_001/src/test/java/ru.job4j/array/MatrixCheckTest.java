c0fdee954a6d57b102a144e0e75e720e12c686e9
johnom-commits <johnom2012@yandex.ru>
2019-09-01 09:24:06 +0300

    MatrixCheck2

--- chapter_001/src/test/java/ru.job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru.job4j/array/MatrixCheckTest.java
@@ -54,4 +54,15 @@ public class MatrixCheckTest {
         boolean result = check.mono(input);
         assertThat(result, is(false));
     }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse2() {
+        MatrixCheck check = new MatrixCheck();
+        boolean[][] input = new boolean[][] {
+                {true, false},
+                {false, true}
+        };
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
 }
