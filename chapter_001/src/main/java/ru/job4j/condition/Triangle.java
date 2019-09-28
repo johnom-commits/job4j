bd67c11368e8971b69de49532b021fedf0834f7c
johnom-commits <johnom2012@yandex.ru>
2019-09-29 00:43:03 +0300

    Triangle new

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,6 +1,15 @@
 package ru.job4j.condition;
 
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
     /**
      * Метод вычисления полупериметра по длинам сторон.
      * Формула.
@@ -30,17 +39,17 @@ public class Triangle {
      * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
      * @return Вернуть площадь, если треугольник существует или -1.
      */
-    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+    public double area(Point first, Point second, Point third) {
         double rsl = -1;
-//        double a = new Point().distance(x1, y1, x2, y2);
-//        double b = new Point().distance(x2, y2, x3, y3);
-//        double c = new Point().distance(x1, y1, x3, y3);
-//        double p = period(a, b, c);
-//        if (this.exist(a, b, c)) {
-//            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-//        } else {
-//            rsl = -1;
-//        }
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        if (this.exist(a, b, c)) {
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        } else {
+            rsl = -1;
+        }
         return rsl;
     }
 }
--- chapter_001/src/test/java/ru.job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru.job4j/condition/TriangleTest.java
@@ -9,10 +9,13 @@ import static org.junit.Assert.*;
 public class TriangleTest {
     @Test
     public void whenAreaSetThreePointsThenTriangleArea() {
+        Point ap = new Point(0, 0);
+        Point bp = new Point(0, 2);
+        Point cp = new Point(2, 0);
         // Создаем объект треугольник.
-        Triangle triangle = new Triangle();
+        Triangle triangle = new Triangle(ap, bp, cp);
         // Вычисляем площадь.
-        double result = triangle.area(0, 0, 0, 2, 2, 0);
+        double result = triangle.area(ap, bp, cp);
         // Задаем ожидаемый результат.
         double expected = 2D;
         //Проверяем результат и ожидаемое значение.
@@ -21,10 +24,13 @@ public class TriangleTest {
 
     @Test
     public void whenAreaSetThreePointsThenTriangleArea2() {
+        Point ap = new Point(1, 1);
+        Point bp = new Point(1, 4);
+        Point cp = new Point(4, 1);
         // Создаем объект треугольник.
-        Triangle triangle = new Triangle();
+        Triangle triangle = new Triangle(ap, bp, cp);
         // Вычисляем площадь.
-        double result = triangle.area(1, 1, 1, 4, 4, 1);
+        double result = triangle.area(ap, bp, cp);
         // Задаем ожидаемый результат.
         double expected = 4.5D;
         //Проверяем результат и ожидаемое значение.
