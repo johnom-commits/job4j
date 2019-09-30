b4f3d453a8f8004117c01f37b8507c3cc694d293
johnom-commits <johnom2012@yandex.ru>
2019-09-30 13:46:34 +0300

    Перезагрузка конструктора

--- chapter_002/src/main/java/ru/job4j/oop/Animal.java
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -1,11 +1,14 @@
 package ru.job4j.oop;
 
 public class Animal {
-    public Animal() {
+    public String name;
+
+    public Animal(String name) {
         super();
+        this.name = name;
         System.out.println("load Animal");
     }
     public static void main(String[] args) {
-        Tiger tiger = new Tiger();
+        Tiger tiger = new Tiger("Уссурийский тигр");
     }
 }
