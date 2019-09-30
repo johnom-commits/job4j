122e3ab7ab19f7fb4ec2e5d82f45f05b825b7805
johnom-commits <johnom2012@yandex.ru>
2019-09-30 09:53:28 +0300

    Конструкторы

---
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Animal {
+    public Animal() {
+        super();
+        System.out.println("load Animal");
+    }
+    public static void main(String[] args) {
+        Tiger tiger = new Tiger();
+    }
+}
