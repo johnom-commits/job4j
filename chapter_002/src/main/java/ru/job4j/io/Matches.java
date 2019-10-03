253c27b515a93d625354cf4e04f83dd2df8a39ab
johnom-commits <johnom2012@yandex.ru>
2019-10-03 22:27:16 +0300

    Matches

---
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int count = 11;
+        int counter = 0;
+        String player = "";
+        while (count > 0) {
+            counter++;
+            if (counter % 2 == 0) {
+                player = "Второй игрок: ";
+            } else player = "Первый игрок: ";
+            System.out.println(player);
+            int matches = Integer.valueOf(input.nextLine());
+            if (matches < 1 || matches > 3 || matches > count) {
+                System.out.println("Число спичек выбрано неверно. Попробуйте еще раз.");
+                counter--;
+                continue;
+            }
+            count = count - matches;
+            System.out.println("Осталось карт: " + count);
+        }
+        System.out.println("Игра окончена. Выиграл " + player);
+    }
+}
