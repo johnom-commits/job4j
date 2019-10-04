8df7d5df14896280422ecbc49d7384a5f7c9e1ce
johnom-commits <johnom2012@yandex.ru>
2019-10-04 17:37:15 +0300

    Transport

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void passengers(int value) {
+
+    }
+
+    @Override
+    public double filling(int fuel) {
+        return 0;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,7 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void go();
+    void passengers(int value);
+    double filling(int fuel);
+}
