e7b2967f44e8a70017e3eec48625e493070c72f7
johnom-commits <johnom2012@yandex.ru>
2019-10-11 13:31:32 +0300

    9.Написать тесты на StartUI

---
+++ chapter_002/src/test/java/ru/job4j/tracker/UserActionTests.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class UserActionTests {
+    @Test
+    public void whenAddItem() {
+        String[] answers = {"Fix PC"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        CreateAction action = new CreateAction();
+        action.execute(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName(), is(expected.getName()));
+    }
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                item.getId(), // id сохраненной заявки в объект tracker.
+                "replaced item"
+        };
+        ReplaceAction replaceAction = new ReplaceAction();
+        replaceAction.execute(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new name");
+        tracker.add(item);
+        String[] answers = {item.getId()};
+        DeleteAction deleteAction = new DeleteAction();
+        deleteAction.execute(new StubInput(answers), tracker);
+        Object expected = null;
+        assertThat(tracker.findById(item.getId()), is(expected));
+    }
+}
