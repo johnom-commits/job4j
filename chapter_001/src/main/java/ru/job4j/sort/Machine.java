1190361c6844c079e4083931217bdd3f39746c7a
johnom-commits <johnom2012@yandex.ru>
2019-10-21 23:30:05 +0300

    bugfix coffee machine

--- chapter_001/src/main/java/ru/job4j/sort/Machine.java
+++ chapter_001/src/main/java/ru/job4j/sort/Machine.java
@@ -3,14 +3,14 @@ package ru.job4j.sort;
 import java.util.Arrays;
 
 public class Machine {
-    private final int[] COINS = {10, 5, 2, 1};
+    private final int[] coins = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int remainder = money - price;
         if (remainder > 0) {
-            for (int i : COINS) {
+            for (int i : coins) {
                 while ((remainder - i) >= 0) {
                     remainder -= i;
                     rsl[size] = i;
