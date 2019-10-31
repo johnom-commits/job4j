f7d1fb32c5a8c0293561a92554c76c864b62d517
johnom-commits <johnom2012@yandex.ru>
2019-10-31 22:23:59 +0300

    sortByAllFields

--- chapter_002/src/main/java/ru/job4j/search/SortUser.java
+++ chapter_002/src/main/java/ru/job4j/search/SortUser.java
@@ -1,24 +1,50 @@
 package ru.job4j.search;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class SortUser {
     public static void main(String[] args) {
         List<User> list = new ArrayList();
         list.add(new User(33, "Ivan"));
-        list.add(new User(30, "Boris"));
-        list.add(new User(29, "Gleb"));
-        Set<User> userSet = sort(list);
-        for (User u : userSet) {
-            System.out.println("Name: " + u.getName() + ", age: " + u.getAge());
-        }
+        list.add(new User(40, "Dermidont"));
+        list.add(new User(30, "Vova"));
+        list.add(new User(29, "Porfiriy"));
+        list.add(new User(37, "Dermidont"));
+        List<User> us = sortNameLength(list);
+        show(us);
+        List<User> us2 = sortByAllFields(list);
+        show(us2);
     }
 
-    public static Set<User> sort(List<User> list) {
-        Set<User> set = new TreeSet<>(list);
-        return set;
+    public static List<User> sortByAllFields(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        }.thenComparing(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getAge() - o2.getAge();
+            }
+        }));
+        return list;
+    }
+
+    public static List<User> sortNameLength(List<User> list) {
+        list.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() - o2.getName().length();
+            }
+        });
+        return list;
+    }
+
+    private static void show(List<User> list) {
+        for (User u : list) {
+            System.out.println("Name: " + u.getName() + ", age: " + u.getAge());
+        }
+        System.out.println();
     }
 }
