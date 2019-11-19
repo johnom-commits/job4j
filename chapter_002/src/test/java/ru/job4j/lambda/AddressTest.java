c3724e0f82e87e05641ea433ad5bc353e5ae418e
johnom-commits <johnom2012@yandex.ru>
2019-11-20 01:26:57 +0300

    Список адресов

---
+++ chapter_002/src/test/java/ru/job4j/lambda/AddressTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class AddressTest {
+    @Test
+    public void listAddressTest() {
+        List<Profile> list = new ArrayList<>();
+        Address address = new Address("Moscow", "Lenina", 12, 6);
+        list.add(new Profile(address));
+        Address address1 = new Address("Moscow", "Polkovaya", 3, 10);
+        list.add(new Profile(address1));
+
+        Profiles profiles = new Profiles();
+        List<Address> result = profiles.collect(list);
+
+        List<Address> expected = new ArrayList<>();
+        expected.add(address);
+        expected.add(address1);
+        assertThat(result, is(expected));
+    }
+}
