b900116389f4480de7c4092df6348d95d927698d
johnom-commits <johnom2012@yandex.ru>
2019-11-21 18:29:13 +0300

    Уникальность элементов и сортировка New

--- chapter_002/src/main/java/ru/job4j/lambda/Profiles2.java
+++ chapter_002/src/main/java/ru/job4j/lambda/Profiles2.java
@@ -7,6 +7,6 @@ import java.util.stream.Collectors;
 public class Profiles2 {
 
     public List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress()).distinct().sorted(Comparator.comparing(o -> o.getCity())).collect(Collectors.toList());
+        return profiles.stream().map(profile -> profile.getAddress()).sorted(Comparator.comparing(o -> o.getCity())).distinct().collect(Collectors.toList());
     }
 }
