f61080ab09a51b69ee43e09f521cf533fbb7a7e6
johnom-commits <johnom2012@yandex.ru>
2019-12-19 21:11:30 +0300

    Создать элементарную структуру дерева

---
+++ junior_chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,80 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+    public Node<E> root;
+    private int modCount = 0;
+
+    public Tree(E value) {
+        root = new Node<>(value);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        Optional<Node<E>> papaOpt = findBy(parent);
+        Optional<Node<E>> son = findBy(child);
+        if (son.isPresent() || papaOpt.isEmpty()) {
+            return false;
+        }
+        Node<E> papa = papaOpt.get();
+        papa.add(new Node<>(child));
+        modCount++;
+        return true;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iter();
+    }
+
+    private class Iter implements Iterator<E> {
+        Queue<Node<E>> data = new LinkedList<>();
+        int expectedModCount = modCount;
+
+        public Iter() {
+            data.offer(root);
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            return !data.isEmpty();
+        }
+
+        @Override
+        public E next() {
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            Node<E> result = null;
+            if (!data.isEmpty()) {
+                result = data.poll();
+                for (Node<E> child : result.leaves()) {
+                    data.offer(child);
+                }
+            }
+            return (result == null) ? null : result.getValue();
+        }
+    }
+}
---
+++ junior_chapter_001/src/test/java/ru/job4j/tree/TreeTest.java
@@ -0,0 +1,83 @@
+package ru.job4j.tree;
+
+import org.junit.Test;
+import java.util.Iterator;
+import static org.junit.Assert.*;
+
+public class TreeTest {
+
+    @Test
+    public void when6ElFindLastThen6() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+        boolean expected = tree.findBy(6).isPresent();
+        assertTrue(expected);
+    }
+
+    @Test
+    public void when6ElFindNotExitThenOptionEmpty() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        boolean expected = tree.findBy(7).isPresent();
+        assertFalse(expected);
+    }
+
+    @Test
+    public void whenTestIteratorNext() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        tree.add(4, 5);
+        tree.add(5, 6);
+
+        Iterator it = tree.iterator();
+        it.next();
+        it.next();
+        it.next();
+        it.next();
+        int expected = (Integer) it.next();
+        assertEquals(expected, 5);
+    }
+
+    @Test
+    public void whenTestIteratorHasNextTrue() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+
+        Iterator it = tree.iterator();
+        it.next();
+        assertTrue(it.hasNext());
+    }
+
+    @Test
+    public void whenTestIteratorNextNull() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+
+        Iterator it = tree.iterator();
+        it.next();
+        it.next();
+        it.next();
+        assertNull(it.next());
+    }
+
+    @Test
+    public void whenTestIteratorHasNextFalse() {
+        Tree<Integer> tree = new Tree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+
+        Iterator it = tree.iterator();
+        it.next();
+        it.next();
+        it.next();
+        assertFalse(it.hasNext());
+    }
+}
