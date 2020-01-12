894a7d61cb792e668dedb5612933ca8ef59fd555
johnom-commits <johnom2012@yandex.ru>
2020-01-12 21:43:42 +0300

    Консольный чат NEW

--- junior_chapter_002/src/main/java/io/Chat.java
+++ junior_chapter_002/src/main/java/io/Chat.java
@@ -1,46 +1,82 @@
 package io;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Chat {
     private static final int MAX_LINE = 4000;
+    private List<String> phases = new ArrayList<>(MAX_LINE);
+    private List<String> listLog = new ArrayList<>();
     private static final String WAY_PHRASE = "C:/projects/phrases.txt";
     private static final String WAY_LOG = "C:/projects/log.txt";
+    private static final String STOP = "Стоп";
+    private static final String CONTINUE = "Продолжить";
+    private static final String FINISH = "Закончить";
 
     public static void main(String[] args) throws IOException {
-        try (var out = new PrintWriter(new FileOutputStream(WAY_LOG))) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Введите сообщение:");
-            Boolean stop = false;
+        Chat main = new Chat();
+        main.readPhrases();
 
-            String str = br.readLine();
-            logging(out, str, " User: ");
-            do {
-                if (!stop) {
-                    Files.lines(Paths.get(WAY_PHRASE)).skip(rnd()).limit(1).peek(s -> logging(out, s, " Bot:  ")).forEach(System.out::println);
-                }
-                str = br.readLine();
-                if (str.equals("Стоп")) {
-                    stop = true;
-                }
-                if (str.equals("Продолжить")) {
-                    stop = false;
-                }
-                logging(out, str, " User: ");
-            } while (!str.equals("Закончить"));
+        var br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Введите сообщение:");
+        Boolean stop = false;
+        String str;
+        do {
+            str = br.readLine();
+            if (str.equals(STOP)) {
+                stop = true;
+            }
+            if (str.equals(CONTINUE)) {
+                stop = false;
+            }
+            main.logging(str, " User: ");
+            if (!stop && !str.equals(FINISH)) {
+                main.printPhrase();
+            }
+        } while (!str.equals(FINISH));
+        main.writeLog();
+    }
+
+    private void readPhrases() {
+        try (var stream = Files.lines(Paths.get(WAY_PHRASE))) {
+            phases = stream.collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private void printPhrase() {
+        var phrase = phases.get(rnd());
+        System.out.println(phrase);
+        logging(phrase, " Bot:  ");
     }
 
     private static int rnd() {
         return (int) (Math.random() * MAX_LINE);
     }
 
-    private static void logging(PrintWriter out, String str, String source) {
-        out.println(new StringBuilder(new Date().toString())
+    private void logging(String str, String source) {
+        var textLog = new StringBuilder(new Date().toString())
                 .append(source)
-                .append(str));
+                .append(str);
+        listLog.add(textLog.toString());
+    }
+
+    private void writeLog() {
+        Charset utf8 = StandardCharsets.UTF_8;
+        try {
+            Files.write(Paths.get(WAY_LOG), listLog, utf8, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        } catch (IOException x) {
+            System.err.format("IOException: %s%n", x);
+        }
     }
 }
