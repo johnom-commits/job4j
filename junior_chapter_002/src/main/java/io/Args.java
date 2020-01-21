72ad72ba0b2b6a5d8e0ab9f708c8aafe7a7940b5
johnom-commits <johnom2012@yandex.ru>
2020-01-22 00:55:51 +0300

    Архивировать проект 5

--- junior_chapter_002/src/main/java/io/Args.java
+++ junior_chapter_002/src/main/java/io/Args.java
@@ -1,11 +1,14 @@
 package io;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Args {
     Map<String, String> mapArgs = new HashMap<>();
     private static final int LENGTH_ARGS = 6;
+    private static final List<String> KEYS = Arrays.asList("-d", "-e", "-o");
 
     public Args(String[] args) {
         parsingArgs(args);
@@ -19,7 +22,7 @@ public class Args {
             arg = args[i];
             int mod = i % 2;
             if (mod == 0) {
-                if (!arg.startsWith("-")) {
+                if (checkArg(arg)) {
                     throw new IllegalArgumentException("Не верный ключ: " + arg);
                 }
                 key = arg;
@@ -30,6 +33,10 @@ public class Args {
                 mapArgs.put(key, arg);
             }
         }
+    }
+
+    private boolean checkArg(String arg) {
+        return KEYS.stream().noneMatch(e -> e.equals(arg));
     }
 
     private void checkLengthArrayArgs(String[] args) {
