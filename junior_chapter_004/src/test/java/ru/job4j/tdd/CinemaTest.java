804e140397639b4037dd525d376d22d4c192a3bf
johnom-commits <johnom2012@yandex.ru>
2020-06-12 18:39:31 +0300

    Тесты сервиса кинотеатра

---
+++ junior_chapter_004/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -0,0 +1,82 @@
+package ru.job4j.tdd;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CinemaTest {
+    @Ignore
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Ignore
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+    // при покупке билета любитель синематографа не выбрал места
+    @Ignore
+    @Test(expected = NullPointerException.class)
+    public void whenBuyWithoutPlaces() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 0, 0, date);
+        assertNull(ticket);
+    }
+
+    // пользователь не нашёл нужный сеанс
+    @Ignore
+    @Test(expected = NullPointerException.class)
+    public void whenDoNotFoundSession() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> false);
+        assertNull(sessions);
+    }
+
+    // когда пользователь указал неправильную дату
+    @Ignore
+    @Test(expected = NullPointerException.class)
+    public void whenSetWrongDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2002, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 4, 4, date);
+        assertNull(ticket);
+    }
+
+    // пользователь приобрёл два билета на места для поцелуев
+    @Ignore
+    @Test(expected = NullPointerException.class)
+    public void whenBuyTwoTickets() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        List<Ticket> ticketList = new ArrayList<>();
+        ticketList.add(cinema.buy(account, 20, 11, date));
+        ticketList.add(cinema.buy(account, 20, 12, date));
+        assertEquals(2, ticketList.size());
+    }
+}
